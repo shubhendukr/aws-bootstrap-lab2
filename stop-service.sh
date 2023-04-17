c2a1a73a86746d6aef18fa147c60fbb918e0cdc9
Arham Saeed <saeedarham100@gmail.com>
2023-04-17 06:08:25 +0500

    Update stop-service.sh

--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -d "/home/ec2-user/app/release" ] && \
-cd /home/ec2-user/app/release && \
+[ -d "/home/ec2-user/app" ] && \
+cd /home/ec2-user/app && \
 npm stop
